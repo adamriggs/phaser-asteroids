4f15fcc0fbe43c92230922929ee689b4e2c9f7fe
Adam Riggs <phlux1111@gmail.com>
2018-02-18 13:45:17 -0500

    all the boys

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
 	game.load.image('ship', 'images/assets/ship.png');
 	game.load.image('marky', 'images/assets/marky.png');
+    game.load.image('donny', 'images/assets/donny.png');
+    game.load.image('pauly', 'images/assets/pauly.png');
 	game.load.image('bullet', 'images/assets/bullets.png');
 	game.load.image('space', 'images/assets/space.jpg');
 }
 
 var ship;
 var marky;
+var donny;
+var pauly;
 var cursors;
 
 var bullet;
@@ -29,6 +33,8 @@ function create() {
 
 	bullets = game.add.group();
     bullets.enableBody = true;
+
+    // I think this does nothing but it needs further investigation
     //bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
     bullets.createMultiple(40, 'bullet');
@@ -37,18 +43,28 @@ function create() {
 
     ship = game.add.sprite(372, 268, 'ship');
     ship.anchor.set(0.5);
+
+    // I think this does nothing but it needs further investigation
     //ship.physicsBodyType = Phaser.Physics.ARCADE;
 
     marky = game.add.sprite(0, 0, 'marky');
     marky.enableBody = true;
 
-    game.physics.enable([ship, marky], Phaser.Physics.ARCADE);
+    donny = game.add.sprite(0, 0, 'donny');
+    donny.enableBody = true;
+
+    pauly = game.add.sprite(0, 0, 'pauly');
+    pauly.enableBody = true;
+
+    game.physics.enable([ship, marky, donny, pauly], Phaser.Physics.ARCADE);
     game.physics.enable(bullets, Phaser.Physics.ARCADE);
 
     ship.body.drag.set(100);
     ship.body.maxVelocity.set(200);
 
-    marky.body.velocity.setTo(2, 60);
+    marky.body.velocity.setTo(Math.random() * 2, (Math.random() * 10) + 60);
+    donny.body.velocity.setTo(Math.random() * 2, (Math.random() * 10) + 60);
+    pauly.body.velocity.setTo(Math.random() * 2, (Math.random() * 10) + 60);
 
     cursors = game.input.keyboard.createCursorKeys();
     game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
@@ -60,17 +76,19 @@ function update() {
 
     screenWrap(ship);
     screenWrap(marky);
-
+    screenWrap(donny);
+    screenWrap(pauly);
     bullets.forEachExists(screenWrap, this);
 
-    game.physics.arcade.overlap(marky, bullets, overlapHandler, processHandler, this);
+    game.physics.arcade.overlap([marky, donny, pauly], bullets, overlapHandler, processHandler, this);
 }
 
 function overlapHandler() {
     console.log('overlap');
 }
 
-function processHandler() {
+function processHandler(event) {
+    console.log(event);
     console.log('process');
     return false;
 }
